ee5f3de2fae4dfad94acccf8f0267cc1fa6b6fe1
Martine Dowden <martine@dowden.us>
2019-06-19 15:40:13 +0200

    step 1

--- app/script.js
+++ app/script.js
@@ -2,4 +2,26 @@
   'use strict';
   console.log('loaded');
 
+  class Blink extends HTMLElement {
+    constructor() {
+      super();
+
+      //  Create a shadow root
+      const shadow = this.attachShadow({
+        mode: 'open'
+      });
+
+      const wrapper = document.createElement('span');
+      const text = this.getAttribute('text');
+      wrapper.textContent = text;
+
+      //  Append
+      shadow.appendChild(wrapper);
+
+    }
+    
+  }
+
+  customElements.define('app-blink', Blink);
+
 })();
